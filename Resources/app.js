b1ba9b535b6ea7bf6ecedd6060e04fc29e0f9f8f
Igor Santos <igorsantos07@gmail.com>
2012-11-12 01:09:36 -0400

    * moving lib files to Resources folder, the right place - removing Underscore and Date requirements from app.js as it does not work. We will need to require them each time the file is needed. refs #6 ! fixing some stuff on calling the initial window

--- Resources/app.js
+++ Resources/app.js
@@ -1,11 +1,8 @@
-// Importing some basic dependencies for the project
-require ("lib/date")
-var _ = require("lib/underscore")
-
 /*
  * TODO: this is only a placeholder until we can securely assure the user has already logged in before
  */
 var hasUserCredentials = false,
-	initialWindow = "ui/common/windows/"+((hasUserCredentials)? "home" : "login")
+	initialWindow = "ui/common/windows/"+((hasUserCredentials)? "home" : "login"),
+	win = require(initialWindow)
 
-require(initialWindow).open()
+new win().open()
