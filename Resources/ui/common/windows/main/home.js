43074d8e2f649ca674308bd04a295737741706ca
Igor Santos <igorsantos07@gmail.com>
2012-11-13 16:49:16 -0400

    + adding tabGroup property to all tabs + adding logout event. refs #28

--- Resources/ui/common/windows/main/home.js
+++ Resources/ui/common/windows/main/home.js
@@ -1,14 +1,25 @@
 module.exports = function() {
 	var ui = require("ui/common/components/defaults")
-	var tabs = Ti.UI.createTabGroup(),
-		tabLists	= new require('ui/common/windows/main/lists')(),
-		tabGroups	= new require('ui/common/windows/main/groups')(),
-		tabSettings	= new require('ui/common/windows/main/settings')()
-		
-	tabs.addTab(tabLists.containingTab)
-	tabs.addTab(tabGroups.containingTab)
-	tabs.addTab(tabSettings.containingTab)
-	tabs.setActiveTab(0)
+	var tabGroup = Ti.UI.createTabGroup()
 	
-	return tabs
+	var tabLists = new require('ui/common/windows/main/lists')()
+	tabLists.containingTab.tabGroup = tabGroup
+	tabGroup.addTab(tabLists.containingTab)
+	
+	var tabGroups = new require('ui/common/windows/main/groups')()
+	tabGroups.containingTab.tabGroup = tabGroup
+	tabGroup.addTab(tabGroups.containingTab)
+	
+	var tabSettings = new require('ui/common/windows/main/settings')()
+	tabSettings.containingTab.tabGroup = tabGroup
+	tabGroup.addTab(tabSettings.containingTab)
+	
+	tabGroup.setActiveTab(0)
+	
+	tabGroup.addEventListener('logout', function() {
+		var login = new require('ui/common/windows/initial/login')()
+		login.open()
+	})
+	
+	return tabGroup
 }
