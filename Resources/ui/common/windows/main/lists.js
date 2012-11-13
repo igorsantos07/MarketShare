313fa0ed1d3a71ef6ea7242b5c7d9faf7be361d0
Igor Santos <igorsantos07@gmail.com>
2012-11-13 00:12:08 -0400

    + adding stub lists to lists list (lol). refs #16

--- Resources/ui/common/windows/main/lists.js
+++ Resources/ui/common/windows/main/lists.js
@@ -2,7 +2,25 @@ module.exports = function() {
 	var ui = require("ui/common/components/defaults")
 	
 	var win = ui.createTabWindow('listsTab')
-	win.add(Ti.UI.createLabel({text:"lists"}))
 	
+		var openLists		= ui.createTableViewSection('openLists'),
+			pendingLists	= ui.createTableViewSection('pendingLists'),
+			archivedLists	= ui.createTableViewSection('archivedLists')
+	
+			openLists.add(ui.createTableViewRow({ title: "Shopping List" }))
+			openLists.add(ui.createTableViewRow({ title: "Weekend Party List" }))
+
+			pendingLists.add(ui.createTableViewRow({ title: "Last week shopping" }))
+			pendingLists.add(ui.createTableViewRow({ title: "Last week party" }))
+			pendingLists.add(ui.createTableViewRow({ title: "Travel to Montreal" }))
+			
+			archivedLists.add(ui.createTableViewRow({ title: "Shopping 04/11" }))
+			archivedLists.add(ui.createTableViewRow({ title: "Shopping 11/11" }))
+			archivedLists.add(ui.createTableViewRow({ title: "Truro Party" }))
+			archivedLists.add(ui.createTableViewRow({ title: "Travel to Québec" }))
+			archivedLists.add(ui.createTableViewRow({ title: "Arrival shopping" }))
+			
+		win.add(ui.createTableView({ data: [openLists, pendingLists, archivedLists] }))
+		
 	return win
 }
