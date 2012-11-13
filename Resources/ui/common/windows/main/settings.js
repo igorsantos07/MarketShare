134d6e4c31f0b1e28f38f469b7d14a1b42ba1df3
Igor Santos <igorsantos07@gmail.com>
2012-11-13 16:56:31 -0400

    + adding a text saying this screen is far from finished and a logout button. refs #21 and #28

--- Resources/ui/common/windows/main/settings.js
+++ Resources/ui/common/windows/main/settings.js
@@ -2,8 +2,14 @@ module.exports = function() {
 	var ui = require("ui/common/components/defaults"),
 		spacing = 10
 	
-	var win = ui.createTabWindow('settingsTab')
-	win.add(Ti.UI.createLabel({text:"settings"}))
+	var win = ui.createTabWindow('settingsTab', { layout: "vertical" })
+	win.add(Ti.UI.createLabel({text:"could this screen be replaced by default android settings screen?", top: 20, width: '80%' }))
+	
+	var logout = ui.createButton({title: 'logout', top: 70})
+	logout.addEventListener('click', function() {
+		win.containingTab.tabGroup.fireEvent('logout')
+	})
+	win.add(logout)
 	
 	return win
 }
