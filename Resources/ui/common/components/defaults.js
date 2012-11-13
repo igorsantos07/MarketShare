445fbe0261a8eb0890edfe45a10ff3a0c59b238c
Igor Santos <igorsantos07@gmail.com>
2012-11-13 12:10:42 -0400

    + adding a helper method to create menus

--- Resources/ui/common/components/defaults.js
+++ Resources/ui/common/components/defaults.js
@@ -36,6 +36,25 @@ exports.createTabWindow = function(titleid, icon, properties) {
 	return win
 }
 
+/**
+ * @param win the window to receive the menu items
+ * @param menuItems an array of menu objects. Each object can have the following properties:
+ *   titleid: the i18n key for its text
+ *   icon: [optional] path to an image
+ *   click: a callback function for the click event
+ */
+exports.setMenu = function(win, items) {
+	win.activity.onCreateOptionsMenu = function(e) {
+		var menu = e.menu
+		for(i in items) {
+			var item = items[i],
+				entry = menu.add({ title: L(item.titleid) })
+			if (item.icon != undefined) entry.setIcon(item.icon)
+			entry.addEventListener('click', item.click);
+		}
+	}
+}
+
 /********************************************* TABLE STUFF *********************************************/
 
 exports.createTableView = function(properties) {
