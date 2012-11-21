34ef7eabb0b0cfa52d56ea73be22cf1cbd55bf5e
Igor Santos <igorsantos07@gmail.com>
2012-11-21 10:20:09 -0400

    + home.js doesn't exist anymore, the main window is now Lists

--- Resources/app.js
+++ Resources/app.js
@@ -2,7 +2,7 @@
  * TODO: this is only a placeholder until we can securely assure the user has already logged in before
  */
 var hasUserCredentials = false,
-	initialWindow = 'ui/common/windows/'+((hasUserCredentials)? 'main/home' : 'initial/login'),
+	initialWindow = 'ui/common/windows/'+((hasUserCredentials)? 'main/lists' : 'initial/login'),
 	win = require(initialWindow)
 
 new win().open()
