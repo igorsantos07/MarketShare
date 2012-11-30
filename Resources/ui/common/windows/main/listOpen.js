2592f7d2abb096c147ca2cd9f504ee519c22e866
Igor Santos <igorsantos07@gmail.com>
2012-11-30 01:48:49 -0400

    + finishing implementation of create list, open list, and the list is opening with real title. closes #16. refs #17

--- Resources/ui/common/windows/main/listOpen.js
+++ Resources/ui/common/windows/main/listOpen.js
@@ -1,51 +1,77 @@
-module.exports = function() {
-	var ui = require("ui/common/components/all")
+module.exports = function(args) {
+	var _ = require('lib/underscore-1.4.2')._,
+		ui = require('ui/common/components/all'),
+		List = require('models/List')
 	
-	var win = ui.createMainWindow('', { title: 'My Shopping List' })
+	if (_.isObject(args[0])) {
+		var list = args[0],
+			win = ui.createMainWindow('', { title: list.name })
+			/* Had to create the win object here because, for some reason, the title was
+			 * only being overrided in the addListData function when called as callback (maybe
+			 * the window object was still being instatiated by Titanium when the code get to
+			 * win.setTitle()?) */
+	}
+	else {
+		var id = args[0],
+			win = ui.createMainWindow('loading')
+	}
 	
-		var table = ui.createTableView()
-	
-			table.appendRow(ui.createTableViewRow({ title: "Item 1" }))
-			table.appendRow(ui.createTableViewRow({ title: "Item 2" }))
-			table.appendRow(ui.createTableViewRow({ title: "Item 3" }))
+	var addListData = function(list) {
+		win.title = list.name
+		if (_.isArray(list.products) && list.products.length > 0) {
+			var tableData
+			_.each(list.products, function(product) {
+				tableData.push({ title: product.name, productId: product.id })
+			})
+			var table = ui.createTableView({ data: tableData })
+		}
+	}
 		
-		win.add(table)
+	if (list) {
+		addListData(list)
+	}
+	else {
+		var list = new List()
+		list.findById(id, function(list) {
+			addListData(list)
+		})
+	}
 		
-		ui.setMenu(win, [
-			{
-				itemId: 1,
-				titleid: 'editName',
-				icon: Ti.Android.R.drawable.ic_menu_edit,
-				click: function(e) { alert("Should prompt user for a new name") }
-			},
-			{
-				itemId: 2,
-				titleid: 'newItem',
-				icon: Ti.Android.R.drawable.ic_menu_add,
-				click: function(e) { ui.goTo('main/listNewItem') }
-			},
-			{
-				itemId: 3,
-				titleid: 'newGroup',
-				icon: Ti.Android.R.drawable.ic_menu_delete,
-				hidden: true,
-				click: function(e) { 
-					var confirm = Ti.UI.createAlertDialog({
-						titleid: 'sure',
-						messageid: 'confirmDeleteList',
-						buttonNames: [L('yes'), L('no')],
-						persistent: true,
-						cancel: 1
-					})
-					confirm.addEventListener('click', function(e) {
-						if (e.cancel === e.index || e.cancel === true) return
-						
-						if (e.index == 0) alert("should remove this list")
-					})
-					confirm.show()
-				}
+	ui.setMenu(win, [
+		{
+			itemId: 1,
+			titleid: 'editName',
+			icon: Ti.Android.R.drawable.ic_menu_edit,
+			click: function(e) { alert("Should prompt user for a new name") }
+		},
+		{
+			itemId: 2,
+			titleid: 'newItem',
+			icon: Ti.Android.R.drawable.ic_menu_add,
+			click: function(e) { ui.goTo('main/listNewItem') }
+		},
+		{
+			itemId: 3,
+			titleid: 'RemoveList',
+			icon: Ti.Android.R.drawable.ic_menu_delete,
+			hidden: true,
+			click: function(e) { 
+				var confirm = Ti.UI.createAlertDialog({
+					titleid: 'sure',
+					messageid: 'confirmDeleteList',
+					buttonNames: [L('yes'), L('no')],
+					persistent: true,
+					cancel: 1
+				})
+				confirm.addEventListener('click', function(e) {
+					if (e.cancel === e.index || e.cancel === true) return
+					
+					if (e.index == 0) alert("should remove this list")
+				})
+				confirm.show()
 			}
-		])
-		
+		}
+	])
+	
 	return win
 }
--- Resources/ui/common/windows/main/lists.js
+++ Resources/ui/common/windows/main/lists.js
@@ -1,35 +1,62 @@
 module.exports = function() {
-	var ui = require("ui/common/components/all")
+	var _ = require('lib/underscore-1.4.2')._,
+		ui = require('ui/common/components/all'),
+		List = require('models/List')
 	
-	var win = ui.createMainWindow('listsWindow')
+	var win = ui.createMainWindow('listsWindow', { exitOnClose: true })
 
-		//adding fake table
-		var openLists		= ui.createTableViewSection('openLists'),
-			pendingLists	= ui.createTableViewSection('pendingLists'),
-			archivedLists	= ui.createTableViewSection('archivedLists')
-	
-			openLists.add(ui.createTableViewRow({ title: "Shopping List" }))
-			openLists.add(ui.createTableViewRow({ title: "Weekend Party List" }))
-
-			pendingLists.add(ui.createTableViewRow({ title: "Last week shopping" }))
-			pendingLists.add(ui.createTableViewRow({ title: "Last week party" }))
-			pendingLists.add(ui.createTableViewRow({ title: "Travel to Montreal" }))
+		var openLists, pendingLists, archivedLists,
+			lists = new List().findAll({}, function(lists) {
+				if (_.isEmpty(lists)) {
+					win.add(ui.createLabel({
+						textid: 'noListToShow',
+						width: '80%',
+						textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER,
+						color: ui.color.text,
+						font: { fontSize: 20 }
+					}))
+				}
+				else {
+					var thisList
+					openLists		= ui.createTableViewSection('openLists'),
+					pendingLists	= ui.createTableViewSection('pendingLists'),
+					archivedLists	= ui.createTableViewSection('archivedLists')
+					
+					_.each(lists, function(list) {
+						if (list.status)
+							thisList = openLists
+						else
+							thisList = (list.paid)? archivedLists : pendingLists
+						
+						var line = ui.createTableViewRow({ title: list.name, listId: list._id.$oid })
+						thisList.add(line)
+					})
 			
-			archivedLists.add(ui.createTableViewRow({ title: "Shopping 04/11" }))
-			archivedLists.add(ui.createTableViewRow({ title: "Shopping 11/11" }))
-			archivedLists.add(ui.createTableViewRow({ title: "Truro Party" }))
-			archivedLists.add(ui.createTableViewRow({ title: "Travel to Québec" }))
-			archivedLists.add(ui.createTableViewRow({ title: "Arrival shopping" }))
-			
-		win.add(ui.createTableView({ data: [openLists, pendingLists, archivedLists] }))
+					var data = []
+					_.each([openLists, pendingLists, archivedLists], function(list) {
+						if (list.rowCount > 0) data.push(list)
+					})
+					
+					var table = ui.createTableView({ data: data }) 
+					win.add(table)
+					
+					table.addEventListener('click', function(e) {
+						ui.goTo('main/listOpen', e.row.listId)
+					})
+				}
+			})
 		
-		//adding menu
 		ui.setMenu(win, [
 			{
 				itemId: 1,
 				titleid: 'newList',
 				icon: Ti.Android.R.drawable.ic_menu_add,
-				click: function(e) { ui.goTo('main/listOpen') }
+				click: function(e) {
+					var list = new List().save(function(newList) {
+						
+						ui.goTo('main/listOpen', newList)
+					})
+				}
 			},
 			{
 				itemId: 2,
