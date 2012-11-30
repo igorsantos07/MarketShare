33d9afd5319587e49bfea8363d987abe8397baca
Igor Santos <igorsantos07@gmail.com>
2012-11-30 01:46:13 -0400

    + adding List model. closes #48

---
+++ Resources/models/List.js
@@ -0,0 +1,68 @@
+var _ = require('lib/underscore-1.4.2')._,
+	Model = require('models/Model')
+
+function List (idOrProperties, callback) {
+	
+	this.COLLECTION = 'lists'
+	
+	this.fields = [
+		'id',
+		'name',
+		'products',
+		'closed',
+		'paid'
+	]
+
+	switch (typeof(idOrProperties)) {
+		case 'string':
+			if (idOrProperties.length == 24) { //pretty much reliable that this is a mongo ObjectID
+				this.id = idOrProperties
+				this.findById(this.id, function(data) {
+					if (_.isFunction(callback))
+						callback(data)
+				})
+			}
+		break
+		
+		case 'object':
+			this.setFields(idOrProperties)
+			if (_.isFunction(callback)) callback(this)
+		break
+	}
+		
+}
+
+_.extend(List.prototype, Model)
+
+List.prototype.defaultName = function() {
+	var date = new Date(),
+		min = date.getMinutes(),
+		sec = date.getSeconds()
+		
+	return [/*date.getFullYear(), */date.getMonth()+1, date.getDate()].join('-') //with year the name is too long to fit in the title bar
+	        +' '+
+		    [date.getHours(), (min < 10)? '0'+min : min, (sec < 10)? '0'+sec : sec].join(':')
+}
+
+//TODO: Maybe all those overrides could be moved to the Model, using this.COLLECTION?
+
+List.prototype.findById = function(id, callback) {
+	Model.findById(this.COLLECTION, id, callback)
+}
+
+List.prototype.find = function(query, callback) {
+	Model.find(this.COLLECTION, query, callback)
+}
+
+List.prototype.findAll = function(query, callback) {
+	Model.findAll(this.COLLECTION, query, callback)
+}
+
+List.prototype.save = function(callback) {
+	if (!this.name)
+		this.name = this.defaultName()
+		
+	Model.save(this.COLLECTION, this, callback)
+}
+
+module.exports = List
