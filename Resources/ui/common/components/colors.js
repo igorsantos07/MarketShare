617579ea3912176b68cbde51bc041285d77f8842
Igor Santos <igorsantos07@gmail.com>
2012-12-01 18:43:41 -0400

    * using a better color schema for modals

--- Resources/ui/common/components/colors.js
+++ Resources/ui/common/components/colors.js
@@ -1,11 +1,12 @@
 var _ = require('lib/underscore-1.4.2')._,
 	color = {},
-	darkerGray	= '#212021',
-	darkGray	= '#4A4542',
-	gray		= '#B5B6B5',
+	darkerGray		= '#212021',
+	darkGray		= '#404040',
+	lightDarkGray	= '#494949',
+	gray			= '#B5B5B5',
 	//now some default colors just to use variables with everything
-	white		= 'white',
-	black		= 'black'
+	white			= '#F7F7F7',
+	black			= '#000000'
 
 /**
  * @class UI.Colors
@@ -14,10 +15,12 @@ var _ = require('lib/underscore-1.4.2')._,
  */
 
 /** @property {String} bg window background */
-/** @property {String} bgOp to be used as semi-transparent background ("shadow") for hovering windows */
+/** @property {String} bgOp semi-transparent stage for modal windows */
+/** @property {String} bgModal background of modal windows */
 /** @property {String} label field label */
 /** @property {String} text common text */
 /** @property {String} field.bg text field background */
+/** @property {String} field.bgModal modal text field background */
 /** @property {String} field.color text field font color */
 
 switch (Titanium.Platform.name) {
@@ -25,12 +28,14 @@ switch (Titanium.Platform.name) {
 	case 'android':
 		color = {
 			bg: black,
-			bgOp: gray,
+			bgOp: black,
+			bgModal: '#292829',
 			label: white,
-			text: darkGray,
+			text: lightDarkGray,
 			field: {
 				bg: darkerGray,
-				color: white
+				color: white,
+				bgModal: darkGray
 			}
 		}
 	break;
@@ -41,11 +46,13 @@ switch (Titanium.Platform.name) {
 		color = {
 			bg: gray, //probably should be some kind of simple texture?
 			bgOp: black,
-			label: darkGray,
-			text: darkGray,
+			bgModal: darkerGray,
+			label: lightDarkGray,
+			text: lightDarkGray,
 			field: {
 				bg: white,
-				color: darkGray
+				color: lightDarkGray,
+				bgModal: white
 			}
 		}
 	break;
