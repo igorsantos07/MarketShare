d539adee5f6b107785f3285fd93c2cf841912111
Igor Santos <igorsantos07@gmail.com>
2013-03-14 18:57:40 -0300

    - removing logout options. closes #97

--- Resources/ui/common/windows/main/settings.js
+++ Resources/ui/common/windows/main/settings.js
@@ -5,12 +5,6 @@ module.exports = function() {
 	var win = ui.createMainWindow('settingsWindow', { layout: "vertical" })
 	win.add(Ti.UI.createLabel({text:"could this screen be replaced by default android settings screen?", top: 20, width: '80%' }))
 	
-	var logout = ui.createButton({title: 'logout', top: 70})
-	logout.addEventListener('click', function() {
-		win.containingTab.tabGroup.fireEvent('logout')
-	})
-	win.add(logout)
-	
 	ui.setMenu(win, [
 		{
 			itemId: 1,
