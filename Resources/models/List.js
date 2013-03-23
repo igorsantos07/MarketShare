3247f3746a32b24aecabf9195645aa46563a26bd
Igor Santos <igorsantos07@gmail.com>
2013-03-23 17:27:46 -0300

    * cleaning the List model, complying with the new hierarchy. Still needs to adapt specific List methods

--- Resources/models/List.js
+++ Resources/models/List.js
@@ -15,17 +15,13 @@ var _ = require('lib/underscore-1.4.2')._,
  * @param {Function/boolean} callback (optional) A function to be called after {@link Models.List#find}
  * or {@link Models.Model#setFields}. If `false`, will not try to fill the missing fields with a find call.
  */
-function List (idOrProperties, callback) {
-	
-	/** Collection name
-	 * @type {String} */
-	this.COLLECTION = 'lists'
-	
-	/** Collection fields
-	 * @property fields
-	 * @protected
-	 * @type {Array} */
-	this.fields = [
+function List () {
+
+}
+
+var modelData = {
+	COLLECTION: 'lists',
+	fields:  [
 		'id',
 		'group',
 		'name',
@@ -33,38 +29,21 @@ function List (idOrProperties, callback) {
 		'status',
 		'paid',
 		'summary'
-	]
-
-	switch (typeof(idOrProperties)) {
-		case 'string':
-			if (idOrProperties.length == 24) { //pretty much reliable that this is a mongo ObjectID
-				this.id = idOrProperties
-				if (callback !== false && callback !== 0) {
-					this.findById(this.id, function(data) {
-						if (_.isFunction(callback)) callback(data)
-					})
-				}
-			}
-		break
-		
-		case 'object':
-			this.setFields(idOrProperties)
-			if (_.isFunction(callback)) callback(this)
-		break
+	],
+	
+	/** List of constants for statuses. The key is the value used in the database.
+	 * @property STATUSES
+	 * @static */
+	STATUS: {
+		CLOSED	: 0,
+		OPEN	: 1,
+		SHOPPING: 2
 	}
-		
 }
 
-_.extend(List.prototype, Model)
+_.extend(List, _.omit(Model, 'save', 'update'), modelData)
+_.extend(List.prototype, Model, modelData)
 
-/** List of constants for statuses. The key is the value used in the database.
- * @property STATUSES
- * @static */
-List.STATUS = {
-	CLOSED	: 0,
-	OPEN	: 1,
-	SHOPPING: 2
-}
 
 /**
  * @method defaultName
@@ -82,10 +61,11 @@ List.prototype.defaultName = function() {
 		    [date.getHours(), (min < 10)? '0'+min : min, (sec < 10)? '0'+sec : sec].join(':')
 }
 
-List.prototype.addProduct = function(product, callback) {
-	this.update({$push: { products: product }}, callback)
+List.prototype.addProduct = function(product) {
+	return this.update({$push: { products: product }})
 }
 
+// TODO: remove callback from here and implement promisses
 List.prototype.close = function(callback) {
 	/*
 	 * FIXME: should verify if all products have the prices before calling this method, or else it's useless
@@ -93,6 +73,7 @@ List.prototype.close = function(callback) {
 	 */
 	var calculateCosts = function(list, callback) {
 		if (list.products && !_.isEmpty(list.products)) {
+			//FIXME: related to #104: no natural list has group yet, so the callback is not called and the calculation is never over
 			var group = new Group(list.group, function(group) {
 				costs = {}
 				_.each(group.users, function(user) {
@@ -131,56 +112,6 @@ List.prototype.close = function(callback) {
 			this.setFields(newData)
 			calculateCosts(this, callback)
 		})
-}
-
-//TODO: Maybe all those overrides could be moved to the Model, using this.COLLECTION?
-
-/** 
- * @method findById
- * @inheritdoc Models.Model#findById
- */
-List.prototype.findById = function(id, callback) {
-	Model.findById(this.COLLECTION, id, callback)
-}
-
-/** 
- * @method find
- * @inheritdoc Models.Model#find
- */
-List.prototype.find = function(query, callback) {
-	Model.find(this.COLLECTION, query, callback)
-}
-
-/** 
- * @method findAll
- * @inheritdoc Models.Model#findAll
- */
-List.prototype.findAll = function(query, callback) {
-	Model.findAll(this.COLLECTION, query, callback)
-}
-
-/** 
- * @method save
- * @inheritdoc Models.Model#save
- */
-List.prototype.save = function(callback) {
-	if (!this.name)
-		this.name = this.defaultName()
-		
-	Model.save(this.COLLECTION, this, callback)
-}
-
-/** 
- * @method update
- * @inheritdoc Models.Model#update
- */
-List.prototype.update = function(newData, callback) {
-	if (!this.id) throw { type: 'ArgumentError', message: 'The model has no ID' }
-	Model.update(this.COLLECTION, this.id, newData, callback)
-}
-
-List.prototype.remove = function(callback) {
-	Model.remove(this.COLLECTION, this.id, callback)
 }
 
 module.exports = List
