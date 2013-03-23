fa57de82be2d5f2172d451df55ac1b1523c787c3
Igor Santos <igorsantos07@gmail.com>
2013-03-23 15:10:01 -0300

    + finishing user testing

--- Resources/tests/models/User.js
+++ Resources/tests/models/User.js
@@ -1,7 +1,7 @@
 var User = require('models/User')
 
-describe('The user model', function() {
-	var user, promise, insertedPk
+describe('The User model', function() {
+	var user, promise, insertedId
 	
 	/**
 	 * @param {Object} promiser Should put a promise in {@link promise}
@@ -12,14 +12,14 @@ describe('The user model', function() {
 		
 		waitsFor(function() {
 			return !promise.isPending()
-		}, 'Request should have finished', 5000)
+		}, 'Request should have finished', 6000)
 		
 		runs(expectations)
 	}
 	
 	function validateUser(user, rawId, skipProperties) {
-		var id		= rawId? user._id.$oid : user.id,
-			notId	= !rawId? user._id.$oid : user.id
+		var id = rawId? user._id.$oid : user.id,
+			notId = !rawId? user._id : user.id
 		
 		expect(user.name)	.toBeDefined()
 		expect(user.email)	.toBeDefined()
@@ -42,17 +42,29 @@ describe('The user model', function() {
 
 	it('should save', function() { promiseTest(
 		function() {
-			promise = User.saveData({
-				name: 'test',
-				email: 'test07@gmail.com'
-			})
-			.then(function(userData) {
-				user = userData
+			promise = Q.all([
+				User.saveData({
+					name: 'test',
+					email: 'test07@gmail.com'
+				}),
+				(function() {
+					saved = new User()
+					saved.name = 'test'
+					saved.email = 'test07@gmail.com'
+					return saved.save()
+				})()
+			])
+			.spread(function(rawUser, savedUser) {
+				user = {
+					raw: rawUser,
+					saved: savedUser
+				}
 			})
 		},
 		function() {
-			validateUser(user, true)
-			expect(insertedPk = user._id.$oid).toBeTruthy()
+			validateUser(user.raw, true)
+			validateUser(user.saved, false)
+			expect(insertedId = user.saved.id).toBeTruthy()
 		}
 	)})
 
@@ -60,7 +72,7 @@ describe('The user model', function() {
 		function() {
 			promise = Q.all([
 				User.find({name: 'test'}),
-				User.findById(insertedPk),
+				User.findById(insertedId),
 				User.findAll()
 			])
 			.spread(function(userFind, userfindById, userFindAll) {
@@ -73,7 +85,7 @@ describe('The user model', function() {
 		},
 		function() {
 			validateUser(user.find, true)
-			validateUser(user.findById, true)			
+			validateUser(user.findById, true)
 			
 			expect(user.findAll).toEqual(jasmine.any(Array))
 			user.findAll.forEach(function(user) {
@@ -81,181 +93,84 @@ describe('The user model', function() {
 			})
 		}
 	)})
-	
-	
-	
-	/********
-	 
-	
-	/-**
-	 * @param {Object} promiser Should put a promise in {@link promise}
-	 * @param {Object} expectations Should run expect() tests
-	 *-/
-	var promiseTest = function(promiser, expectations) {
-		runs(promiser)
-		
-		waitFor(function() {
-			return !promise.isPending()
-		}, 'Request should have finished', 5000)
-		
-		runs(expectations)
-	}
-	
-	afterEach(function () {
-		user = undefined
-		promise = undefined
-	})
 
-	it('should save', promiseTest(
+	it('should build itself', function() { promiseTest(
 		function() {
-			promise = User.saveData({
-				name: 'test',
-				email: 'test07@gmail.com'
-			})
-			.then(function(userData) {
-				user = userData
+			promise = User.construct(insertedId)
+			.then(function(foundUser) { user = foundUser })
+		},
+		function() { validateUser(user, false) }
+	)})
+	
+	it('should be able to count', function() { promiseTest(
+		function() {
+			promise = User.count().then(function(total) { user = total })
+		},
+		function() {
+			expect(user).toEqual(jasmine.any(Number))
+			expect(user).toBeGreaterThan(0)
+		}
+	)})
+	
+	it('should update', function() { promiseTest(
+		function() {
+			promise = Q.all([
+				User.updateData(insertedId, { name: 'testing' }),
+				(function() {
+					return User.construct(insertedId).then(function(updated) {
+						updated.name = 'testing 2'
+						return updated.update()
+					})
+				})()
+			])
+			.spread(function(rawUser, updatedUser) {
+				user = {
+					raw: rawUser,
+					updated: updatedUser
+				}
 			})
 		},
 		function() {
+			validateUser(user.raw, true, true)
+			expect(user.raw.name).toEqual('testing')
+			expect(user.raw.email).toEqual('test07@gmail.com')
 			
+			validateUser(user.updated, false, true)
+			expect(user.updated.name).toEqual('testing 2')
+			expect(user.updated.email).toEqual('test07@gmail.com')
 		}
-	)) 
-	 */
+	)})
 	
-	
-	
-	
-	
-})
-/*
-var user = new User()	
-user.name = 'testing'
-user.email = 'test07@gmail.com'
-
-
-User.count()
-
-.then(function(total) {
-	Ti.API.error("=======================> Current users: "+total)
-	return true
-})
-
-.then(function() {
-		Ti.API.error("===========> STARTING TESTS <===========")
-		
-		return Q.all([
-			User.find({email: 'igorsantos07@gmail.com' })
-			.then(function(user) {
-				return (
-					user._id.$oid == '50c753bfa6a188c0e7b678b2' &&
-					user.name == 'Igor' &&
-					user.email == 'igorsantos07@gmail.com'
-				)
-			}),
-			
-			User.findById('50c753bfa6a188c0e7b678b2')
-			.then(function(user) {
-				return (
-					user._id.$oid == '50c753bfa6a188c0e7b678b2' &&
-					user.name == 'Igor' &&
-					user.email == 'igorsantos07@gmail.com'
-				)
-			}),
-			
-			User.findAll({email: 'test07@gmail.com' })
-			.then(function(users) {
-				if (users.length > 0) {
-					users.forEach(function(user) {
-						Ti.API.error("===========> FindAll: "+user._id.$oid+' - '+user.name+':'+user.email)	
-					})
+	it('should remove stuff', function() { promiseTest(
+		function() {
+			promise = Q.all([
+				User.remove(insertedId),
+				Q.all([
+					User.saveData({ name: 'removeable', email: 'remove@gmail.com' }),
+					User.saveData({ name: 'removeable', email: 'remove@gmail.com' }),
+					User.saveData({ name: 'removeable', email: 'remove@gmail.com' })
+				]).then(function() { return User.remove({ email: 'remove@gmail.com' }) })
+			])
+			.spread(function(removeOne, removeMany) {
+				user = {
+					one: removeOne,
+					many: removeMany
 				}
-				else {
-					Ti.API.error("===========> FindAll: none")
-				}
-				return true
-			}),
-			
-			User.construct('50c753bfa6a188c0e7b678b2')
-			.then(function(user) {
-				return (
-					user.id == '50c753bfa6a188c0e7b678b2' &&
-					user.name == 'Igor' &&
-					user.email == 'igorsantos07@gmail.com'
-				)
-			}),
-			
-			
-			}),
-			
-			User.updateData('50c753bfa6a188c0e7b678b2', {name: 'Igor Santos'})
-			.then(function(user) {
-				return (
-					user.name == 'Igor Santos' &&
-					user.email == 'igorsantos07@gmail.com' &&
-					user.id == undefined &&
-					user._id.$oid == '50c753bfa6a188c0e7b678b2'
-				)
-			}),
-			
-			user.save()
-			.then(function(saved) {
-				return (
-					saved.name == 'testing' &&
-					saved.email == 'test07@gmail.com' &&
-					!_.empty(user.id)
-				)
 			})
-			.then(function() {
-				user.name = 'update'+_.random(100)
-				return user.update()
-			})
-			.then(function(saved) {
-				Ti.API.error("===========> Update: "+saved.name)
-				Ti.API.error("===========> Updated: "+user.id)
-				return true
-			}),
-			
-		])
-		
-		
-		.then(function(results) {
-			results.forEach(function(result, index) {
-				if (result === false)
-					Ti.API.error("===========> Test error: "+index)
-			})
-		})
-
+		},
+		function() {
+			expect(user.one.email).toEqual('test07@gmail.com')
+			expect(user.many.removed).toEqual(3)
+		}
+	)})
+	
+	it('should clean test data', function() { promiseTest(
+		function() {
+			promise = User.remove({email:'test07@gmail.com'})
+			.then(function(removed) { user = removed })
+		},
+		function() {
+			expect(user.removed).toEqual(1)
+		}
+	)})
 })
-
-.then(function() {
-	Ti.API.error("===========> CLEANING TESTS <===========")
-		
-	return Q.all([
-		User.updateData('50c753bfa6a188c0e7b678b2', { '$set': {
-			name: 'Igor',
-			email: 'igorsantos07@gmail.com'
-		}}),
-		
-		User.remove({email: 'test07@gmail.com'})			
-	])
-	.spread(function(update, remove) {
-		if (update) Ti.API.error("===========> Default user fixed: "+update.name)
-		if (remove) Ti.API.error("===========> All test users removed: "+remove.removed)
-		return true
-	})
-})
-
-.then(function() {
-	return User.count().then(function(total) {
-		Ti.API.error("=======================> Current users: "+total)
-		return true
-	})
-})
-
-.fail(function (error) {
-	Ti.API.error("=======================> BANG BANG BANG")
-	Ti.API.error(JSON.stringify(error))
-})
-
-}
-*/
